46c6cc474270196351fa906bc6fe30b1cc8b6eaa
loyer - kmz <oreliauk@gmail.com>
2019-04-08 23:50:28 +0200

    arcade link review

--- src/Konami.js
+++ src/Konami.js
@@ -8,7 +8,7 @@ const Konami = ({onClick}) => (
         <span className="close-display" onClick={() => onClick()}><img src={cross} alt='close' /></span>
     </div>
     <div className="chose-game">
-        <a className="konami-link" href="https://loving-meninsky-ccd2a2.netlify.com/" target="_blank" rel="noopener noreferrer"><div id="game1"></div></a>
+        <a className="konami-link" href="https://aracade-game.netlify.com" target="_blank" rel="noopener noreferrer"><div id="game1"></div></a>
         <a className="konami-link" href="https://pendu-8557ea.netlify.com/" target="_blank" rel="noopener noreferrer"><div id="game2"></div><p className="french">(french word)</p></a>
         
     </div>
